4e852c5fb8e2acd38ff7f3ffe61a45f539ee5097
PhilipArlandAlili <202080139@psu.palawan.edu.ph>
2023-11-11 21:31:22 +0800

    done records app

---
+++ includes/sidebar.php
@@ -0,0 +1,32 @@
+
+<div class="logo">
+    <a href="javascript:;" class="simple-text">
+        ALILI, Philip Arland
+    </a>
+</div>
+<ul class="nav">
+    <li class="nav-item active">
+        <a class="nav-link" href="transaction.php">
+            <i class="nc-icon nc-icon nc-delivery-fast"></i>
+            <p>Transaction</p>
+        </a>
+    </li>
+    <li>
+        <a class="nav-link" href="./office.php">
+            <i class="nc-icon nc-single-copy-04"></i>
+            <p>Office</p>
+        </a>
+    </li>
+    <li>
+        <a class="nav-link" href="./employee.php">
+            <i class="nc-icon nc-badge"></i>
+            <p>Employee</p>
+        </a>
+    </li>
+    <li class="nav-item active active-pro">
+        <a class="nav-link active" href="javascript:;">
+            <i class="nc-icon nc-alien-33"></i>
+            <p>Upgrade plan</p>
+        </a>
+    </li>
+</ul>
